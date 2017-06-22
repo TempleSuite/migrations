6eff60192318b1f0bc8e39bfaa3d7026bb27182f
jeffwalsh <rlxrlps@gmail.com>
2017-06-22 13:56:55 -0700

    add module

---
+++ src/Module.php
@@ -0,0 +1,14 @@
+<?
+
+namespace templesuite\migrations;
+
+use yii\base\Module as BaseModule;
+
+class Module extends BaseModule {
+	public function $controllerNamespace = "templesuite\migrations\controllers";
+	
+	public function init()
+	{
+		parent::init();
+	}
+}
